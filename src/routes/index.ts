64922dd3b53ca5b916b16a9bc37d133d475c88f1
Allan Nubling <69007582+allan-nubling@users.noreply.github.com>
2021-03-21 09:52:07 -0300

    webhooks test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,31 @@
-import { Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express'
 
 import ExampleController from '@controllers/ExampleController'
+import { NotFound } from '@utils/errors'
 
 const { NODE_ENV } = process.env
 const routes = Router()
 
-routes.get('/', async (_req, res) => {
+routes.get('/', async (_req: Request, res: Response) => {
     res.send('⚡️[server]: Server is running')
 })
 
-routes.get('/env', async (_req, res) => {
+routes.all('/webhook', async (req: Request, res: Response) => {
+    console.log(req.method)
+    console.log('body', req.body)
+    console.log('query', req.query)
+    console.log(NODE_ENV)
+    res.send(NODE_ENV)
+})
+
+routes.get('/env', async (req: Request, res: Response) => {
     res.send(NODE_ENV)
 })
 
 routes.post('/example', ExampleController.save)
 
-routes.all('*', (_req, res) => {
-    return res.status(404).json({
-        status: 'error',
-        message: 'route not found'
-    })
+routes.all('*', async (_req: Request, _res: Response, next: NextFunction) => {
+    next(new NotFound('route not found'))
 })
 
 export default routes
